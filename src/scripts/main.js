a27804c7bd5e4018dd6a136cf83d10ada851ba0e
Anton Donchenko <donch.a080@gmail.com>
2025-03-19 23:14:26 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('table tr');
+
+for (const row of rows) {
+  const isHeaderRow = row.querySelector('th') !== null;
+
+  if (isHeaderRow) {
+    const newCell = document.createElement('th');
+
+    newCell.textContent = row.cells[1].textContent;
+
+    row.insertBefore(newCell, row.cells[4]);
+  } else {
+    const newCell = row.insertCell(4);
+
+    newCell.textContent = row.cells[1].textContent;
+  }
+}
